fc30807bf755b2fe5a3455c862f2d445e1432a95
Tom Seifert <dev@tpseifert.de>
2022-02-09 16:26:47 +0100

    Better sample data for ScannedProductsScreen

--- androidApp/src/main/java/de/memorian/av/android/presentation/scannedproducts/ScannedProductsPreviewParameterProvider.kt
+++ androidApp/src/main/java/de/memorian/av/android/presentation/scannedproducts/ScannedProductsPreviewParameterProvider.kt
@@ -12,30 +12,39 @@ class ScannedProductsPreviewParameterProvider :
             listOf(
                 ScannedProductSummary(
                     databaseId = 0,
-                    title = "Test Product",
-                    imageUrl = "https://picsum.photos/200/300",
-                    ean = "1111111111111",
+                    title = "Super Mario Odyssey",
+                    imageUrl = "https://m.media-amazon.com/images/I/91t3mH6psYL._SL1500_.jpg",
+                    ean = "0045496420871",
                     scannedDate = Clock.System.now(),
-                    bestPrice = "24,99€",
-                    bestPlatform = "A good buyer"
+                    bestPrice = "32,30€",
+                    bestPlatform = "ZOXS"
                 ),
                 ScannedProductSummary(
                     databaseId = 0,
-                    title = "Test Product",
-                    imageUrl = "https://picsum.photos/200/300",
-                    ean = "1111111111111",
+                    title = "Star Wars: Der Aufstieg Skywalkers Blu-ray",
+                    imageUrl = "https://assets.mmsrg.com/isr/166325/c1/-/ASSET_MMS_72974753/fee_786_587_png",
+                    ean = "8717418564391",
                     scannedDate = Clock.System.now(),
-                    bestPrice = "24,99€",
-                    bestPlatform = "A good buyer"
+                    bestPrice = "2,60€",
+                    bestPlatform = "reBuy"
                 ),
                 ScannedProductSummary(
                     databaseId = 0,
-                    title = "Test Product",
-                    imageUrl = "https://picsum.photos/200/300",
-                    ean = "1111111111111",
+                    title = "Harry Potter und die Heiligtümer des Todes (Band 7)",
+                    imageUrl = "https://images-na.ssl-images-amazon.com/images/I/51L3ygSkwrL._SX357_BO1,204,203,200_.jpg",
+                    ean = "9783551577771",
                     scannedDate = Clock.System.now(),
-                    bestPrice = "24,99€",
-                    bestPlatform = "A good buyer"
+                    bestPrice = "7,30€",
+                    bestPlatform = "momox"
+                ),
+                ScannedProductSummary(
+                    databaseId = 0,
+                    title = "FIFA 22 - [PlayStation 5]",
+                    imageUrl = "https://assets.mmsrg.com/isr/166325/c1/-/ASSET_MMS_86521422/fee_786_587_png",
+                    ean = "5030948123856",
+                    scannedDate = Clock.System.now(),
+                    bestPrice = "15,00€",
+                    bestPlatform = "rankauf"
                 ),
             )
         )
